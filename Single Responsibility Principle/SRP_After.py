67e777acae275a81d1b7d23f6c1b29da884941d4
aduncanson <aduncanson@hotmail.co.uk>
2020-11-05 21:20:43 +0000

    SRP example

---
+++ Single Responsibility Principle/SRP_After.py
@@ -0,0 +1,48 @@
+''' Class that declares person instance '''
+class Person():
+    ''' The Person class object '''
+
+    # Variables to be identical when instanced
+    listOfAwards = []
+
+    # Variables to be defined when instanced
+    def __init__(self, first_name, surname_name):
+        self.first_name = first_name
+        self.surname_name = surname_name
+
+    # __str__ function
+    def __str__(self):
+        return "BookPublisher(first_name=%s, surname_name=%s)"%(self.first_name, self.surname_name)
+
+    # __repr__ function
+    def __repr__(self):
+        return "BookPublisher(first_name=%s, surname_name=%s)"%(self.first_name, self.surname_name)
+
+''' Class that declares award instance '''
+class Award():
+    ''' The Award class object '''
+
+    # Variables to be defined when instanced
+    def __init__(self, award_name):
+        self.award_name = award_name
+
+    # __str__ function
+    def __str__(self):
+        return "BookPublisher(award_name=%s)"%(self.award_name)
+
+    # __repr__ function
+    def __repr__(self):
+        return "BookPublisher(award_name=%s)"%(self.award_name)
+
+# Add the award to the person's list
+def add_to_awards_list(person, award):
+    ''' Adds award to person's personal list '''
+    print("'%s' added to %s's list."%(award.award_name, person.first_name))
+    person.listOfAwards.append(award)
+
+# Create instances
+person1 = Person("Adam", "Duncanson")
+award1 = Award("Python Award!")
+
+# Add award to person's list
+add_to_awards_list(person1, award1)
